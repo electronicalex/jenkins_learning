6653d63382ec7844dff9f904f8684c7ddc4f1e11
Alex <113867908+electronicalex@users.noreply.github.com>
2023-12-06 11:53:01 +0800

    Update tools.groovy
    rename

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 //格式化输出
-def PrintMes(value,color){
+def printMsg(value,color){
     colors = ['red'   : "\033[1;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[1;34m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'green' : "\033[1;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
